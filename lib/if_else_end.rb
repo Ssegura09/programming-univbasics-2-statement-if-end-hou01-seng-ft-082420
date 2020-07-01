1b9e84f8db527d95f12ff60908d0f3fd39fcc704
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-07-01 21:33:10 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,5 @@
 # Write your solution here
-run_code_inside = false
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
+
 
 chance_of_rain = 0.2
 if chance_of_rain <= 0.25
