2a01b8626314cdbb447fdcb5f217274fc16dac95
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-07-01 21:39:21 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,5 +1,3 @@
-
-  
-  puts "You know what year it is??"
+puts "You know what year it is??"
 this_year = 2019
-puts "Hey, it's 2019!" if this_year == 2019
+puts "Hey, it's not 2019!" unless this_year == 2019
